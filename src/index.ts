7b2c30e2d3b4f5892aaf0202c8e9ac6eda8e661e
Kiko <52105584+HyperKiko@users.noreply.github.com>
2025-08-05 13:02:37 +0200

    let's just see if this works on cf

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,12 @@ import { apiCall } from "./apicall";
 import { isValidInteger } from "./utils";
 import { getUDID } from "./get_udid";
 import { cors } from "hono/cors";
+import * as cheerio from "cheerio";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
+
+const USER_AGENT =
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36";
 
 app.use("*", (c, next) =>
     cors({ origin: c.env.CORS_ORIGINS.split(",") })(c, next)
@@ -131,6 +135,119 @@ app.get("/movie/:id", async (c) => {
             open_udid: getUDID(c.env.MBP_TOKEN)
         })
     );
+});
+
+app.get("/web/movie/:id", async (c) => {
+    const startTime = Date.now();
+    const id_str = c.req.param("id");
+    if (!id_str || !isValidInteger(id_str))
+        return c.json(
+            { type: "error", message: "ID parameter needs to be a number." },
+            400
+        );
+    const id = Number(id_str);
+
+    const $ = cheerio.load(
+        await fetch(`https://www.movieboxpro.app/movie/${id}`, {
+            headers: {
+                "User-Agent": USER_AGENT,
+                Cookie: `ui=${c.env.MBP_TOKEN}`
+            }
+        }).then((res) => res.text())
+    );
+
+    const list = (
+        await Promise.all(
+            $("[oss_download_url]")
+                .map((_, el) => {
+                    const url = new URL(
+                        `https://www.movieboxpro.app${el.attribs.oss_download_url}`
+                    );
+                    const cheerioEl = $(el);
+                    const secondEl = $(cheerioEl.find("> div")[1]);
+                    return fetch(url, {
+                        headers: {
+                            "User-Agent": USER_AGENT,
+                            Cookie: `ui=${c.env.MBP_TOKEN}`
+                        }
+                    })
+                        .then((res) => res.text())
+                        .then((text) => {
+                            const streams = (
+                                JSON.parse(
+                                    /^[ |\t]*var qualityLevels = (.*?);$/m.exec(
+                                        text
+                                    )?.[1] || "[]"
+                                ) as any[]
+                            ).map((stream) =>
+                                stream.sources.map((source: any) => ({
+                                    ...Object.fromEntries(
+                                        Array.from(url.searchParams).map(
+                                            ([key, value]) => [
+                                                key,
+                                                Number(value)
+                                            ]
+                                        )
+                                    ),
+                                    bitstream: cheerioEl
+                                        .find(".speed > span")
+                                        .text()
+                                        .trim(),
+                                    quality: stream.name.replace(" HDR", ""),
+                                    real_quality: stream.name.replace(
+                                        " HDR",
+                                        ""
+                                    ),
+                                    filename: secondEl
+                                        .find("> span")
+                                        .text()
+                                        .trim(),
+                                    dateline: Math.floor(
+                                        new Date(
+                                            secondEl
+                                                .find("> p > span")
+                                                .first()
+                                                .text()
+                                                .trim()
+                                        ).getTime() / 1000
+                                    ),
+                                    size: $(secondEl.find("> p > span")[1])
+                                        .text()
+                                        .trim(),
+                                    original: 0,
+                                    hdr: Number(stream.name.endsWith(" HDR")),
+                                    path:
+                                        source.src !==
+                                        "/static/video/vip_only.mp4"
+                                            ? source.src
+                                            : "",
+                                    format: source.type.split("/")[1]
+                                }))
+                            );
+                            streams
+                                .at(-1)
+                                .forEach(
+                                    (source: any) => (
+                                        (source.original = 1),
+                                        (source.quality = "org")
+                                    )
+                                );
+                            return streams;
+                        });
+                })
+                .toArray()
+        )
+    ).flat(2);
+
+    return c.json({
+        code: 1,
+        message: "success",
+        data: {
+            seconds: Math.floor((Date.now() - startTime) / 1000),
+            quality: list.map((item) => item.real_quality),
+            list
+        }
+    });
 });
 
 app.get("/subtitles/tv/:id/:season/:episode/:stream", async (c) => {
